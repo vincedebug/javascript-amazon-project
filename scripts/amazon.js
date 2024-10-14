a58cd79f1367945c4b03d5fbf29fd9a7f9a2995a
vincedebug <vinceferrerasermino25@gmail.com>
2024-10-14 17:32:49 +0800

    add to cart quantity and also learn how it works

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 let productsHTML = '';
 
+// HTML template for the front
 products.forEach((product) => {
   productsHTML += `
     <div class="product-container">
@@ -45,49 +46,48 @@ products.forEach((product) => {
           Added
         </div>
 
-        <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
+        <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
           Add to Cart
         </button>
     </div>`;
 
 })
 
+// Display each Product in the Webpage
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
-  // const productId = button.dataset.productId; 
-  const productName = button.dataset.productName;
 
+  const productId = button.dataset.productId; 
 
+  console.log(productId);
   let matchingItem;
 
-  //  cart.forEach((item) => {
-  //   if (productId === item.productId) {
-  //     matchingItem = item;
-  //   }
+  cart.forEach((item) => {
+    if (productId === item.productId) {
+      matchingItem = item;
+    }
+  });
 
-  // });
+  if (matchingItem) {
+    matchingItem.quantity +=1
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1
+      });
+  }
 
-  //   if (matchingItem) {
-  //     matchingItem.quantity +=1
-  //   } else {
-  //     cart.push({
-  //       productId: productId,
-  //       quantity: 1
-  //      });
-  //   }
 
-  //   let cartQuantity = 0;
+  // update the cart quantity ui
+  let cartQuantity = 0;
     
-  //   cart.forEach((item) => {
-  //     cartQuantity += item.quantity;
-  //   });
+  cart.forEach((item) => {
+    cartQuantity += item.quantity;
+  });
 
-  //   document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-
-  //   console.log(cartQuantity);
-  //  console.log(cart);
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   });
 });
