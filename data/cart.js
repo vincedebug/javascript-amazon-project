2fb7e8317c84434bde712e584712d6e5fd6bc72e
vincedebug <vinceferrerasermino25@gmail.com>
2024-10-16 17:58:19 +0800

    save cart to local Storage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,18 @@
-export let cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2
-}, {
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}];
+
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2
+  }, {
+  
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];
+}
+
+
 
 export function addToCart(productId) {
 
@@ -28,6 +35,11 @@ export function addToCart(productId) {
       quantity
       });
   }
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
 }
  
 export function removeFromCart(productId) {
@@ -40,4 +52,5 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+  saveToStorage();
 }
